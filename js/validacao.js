666e1eaebf7285302eb99c2454fd7ecfaafa6d20
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-06-09 13:41:41 -0300

    Initial commit

---
+++ js/validacao.js
@@ -0,0 +1,59 @@
+export function valida(input) {
+  const tipoDeInput = input.dataSet.tipo;
+
+  if (validadores[tipoDeInput]) {
+    validadores[tipoDeInput](input);
+  }
+
+  if (input.validity.validi) {
+    input.parentElement.classList.remove("input-container--invalido");
+  } else {
+    input.parentElement.classList.add("input-container--invalido");
+  }
+}
+
+const mensagensDeErro = {
+  nome: {
+    valueMissing: "O campo nome não pode estar vazio.",
+  },
+  email: {
+    valueMissing: "O campo email não pode estar vazio.",
+    typeMismatch: "O email digitado não é valido.",
+  },
+  senha: {
+    valueMissing: "O campo senha não pode estar vazio.",
+    patternMismatch:
+      "A senha deve conter entre 6 a 12 digitos. Deve Conter pelo menos uma letra maiúscula, um número e não deve conter símbolos.",
+  },
+  dataNascimento: {
+    valueMissing: "O campo de data de nascimento não pode estar vazio.",
+    customError: "Você deve ser maior de 18 anos para se cadastrar.",
+  },
+};
+
+const validadores = {
+  dataNascimento: (input) => validaDataNascimento(input),
+};
+
+function validaDataNascimento(input) {
+  const dataRecebida = new Date(input.value);
+  let mensagem = "";
+
+  if (!maiorQue18(dataRecebida)) {
+    mensagem = "Você deve ser maior que 18 anos para se cadastrar.";
+  }
+
+  input.setCustomValidity(mensagem);
+}
+
+function maiorQue18(data) {
+  //fazendo um new Date() sem passar nenhum parametro ele retorna a data atual
+  const dataAtual = new Date();
+  const dataMais18 = new Date(
+    data.getUTCFullYear() + 18,
+    data.getUTCMonth(),
+    data.getUTCDate(),
+  );
+
+  return dataMais18 <= dataAtual;
+}
